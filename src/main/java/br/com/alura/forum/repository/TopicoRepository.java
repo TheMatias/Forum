e7241baa05acea4f3e827fd8f73b87adc49e9666
José Matias <matiasjose547@gmail.com>
2020-02-01 23:52:03 -0300

    add metodo resposavel por retornar uma lista de topicos por nome do titulo em TopicoRepository

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,6 +1,7 @@
 package br.com.alura.forum.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +9,13 @@ import br.com.alura.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+	// metodo responsavel por retornar atributo nomme de curso
+	// se houver um atributo em topico nomeada cursoNome
+	// para se referir ao atributo de nome do curso
+	// sera necessario adicionar o underline entre o nome da classe e do atributo
+	// segue o exemplo..
+	// List<Topico> findByCurso_Nome(String nomeCurso);
 	List<Topico> findByCursoNome(String nomeCurso);
 
-	
+	Optional<Topico> findByTitulo(String titulo);
 }
